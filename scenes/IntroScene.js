fe290012c852a07b5d1fc55a1ee8e26107d3f2f2
Juan Domandl <jdomandl@Juans-MacBook-Pro.local>
2022-03-11 13:15:25 -0300

    names solved

--- scenes/IntroScene.js
+++ scenes/IntroScene.js
@@ -40,7 +40,7 @@ class IntroScene extends Phaser.Scene {
     this.input.addPointer();
     this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     this.text1 = this.add.bitmapText(70, 400, 'carrier_command', 'Putin Must Die!',25);
-    this.text2 = this.add.bitmapText(100, 450, 'carrier_command', 'Stop the invasion with the gosth of kivy',8);
+    this.text2 = this.add.bitmapText(100, 450, 'carrier_command', 'Stop the invasion with the ghost of Kyiv',8);
     this.text2.setCharacterTint(0, 100, true, 0xFFFF00);
     this.text3 = this.add.bitmapText(20, 700, 'carrier_command', 'Press SPACE or touch  \n\nthe screen to start \n\nplaying...', 10);
     this.getRubleDevaluation().then(devaluation => {
